75ea8bc3fadbd13523af3c75527dddbcaf42447f
48038411 <48038411@qq.com>
2019-04-11 06:16:43 +0800

    Java学习目录

---
+++ thread-study/src/com/soft1841/thread/DrawImageTest.java
@@ -0,0 +1,26 @@
+package com.soft1841.thread;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawImageTest extends JFrame {
+    public DrawImageTest(){
+        this.setSize(500,300);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        add(new CanvasTest());
+        this.setTitle("新的方法绘制图像");
+    }
+
+    public static void main(String[] args) {
+        //使窗体可见
+        new DrawImageTest().setVisible(true);
+    }
+    class CanvasTest extends Canvas{
+        public  void paint(Graphics g){
+            super.paint(g);
+            Graphics2D graphics2D = (Graphics2D) g;
+            Image image = new ImageIcon(this.getClass().getResource("/img/1.jpg")).getImage();
+            graphics2D.drawImage(image,0,0,this);
+        }
+    }
+}
